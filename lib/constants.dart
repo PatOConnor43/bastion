b6bb3469fb734e0f5b7fcdc71f02bc4ab8296957
Pat O'Connor <patoconnor43@gmail.com>
2018-10-13 17:27:57 -0700

    profile-navigation: Add constants

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+class RouteConstants {
+  static const String SYMPTOMS = '/symptoms';
+  static const String DOCTORS = '/doctors';
+  static const String FACILITIES = '/facilities';
+  static const String CONDITIONS = '/conditions';
+  static const String MEDICATION = '/medications';
+  static const String PROCEDURES = '/procedures';
+  static const String PROFILE = '/profile';
+  static const String HOTLINES = '/hotlines';
+  static const String NEWS = '/news';
+  static const String ABOUT = '/about';
+  static const String SURVEY = '/survey';
+}
